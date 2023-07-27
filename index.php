c22e84d2815213a560f390ee06d2c75204fb312a
rismankadar <kadarismanfisika@gmail.com>
2023-07-27 16:47:29 +0700

    first commit

---
+++ index.php
@@ -0,0 +1,61 @@
+<!doctype html>
+<html lang="en">
+
+<head>
+      <meta charset="utf-8">
+      <meta name="viewport" content="width=device-width, initial-scale=1">
+      <title>kadarisman</title>
+      <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.0/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-9ndCyUaIbzAi2FUVXJi0CjmCapSmO7SnpJef0486qhLnuZ2cdeRhO02iuK6FUUVM" crossorigin="anonymous">
+      <link rel="stylesheet" href="css/custom.css">
+      <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/animate.css/4.1.1/animate.min.css" />
+</head>
+
+<body>
+      <section>
+
+            <header class="shadow-sm d-flex flex-wrap sticky-top align-items-center justify-content-center justify-content-md-between py-3 mb-4 border-bottom bg-purple animate__animated animate__bounce">
+                  <div class="container">
+                        <div class="col-md-3 mb-2 mb-md-0">
+                              <a href="/" class="d-inline-flex link-body-emphasis text-decoration-none">
+                                    <svg class="bi" width="40" height="32" role="img" aria-label="Bootstrap">
+                                          <use xlink:href="#bootstrap"></use>
+                                    </svg>
+                              </a>
+                        </div>
+
+                        <ul class="nav col-12 col-md-auto mb-2 justify-content-center mb-md-0 fs-5">
+                              <li><a href="#home" class="nav-link px-2 link-secondary">Home</a></li>
+                              <li><a href="#" class="nav-link px-2 link-light">About</a></li>
+                              <li><a href="#" class="nav-link px-2 link-light">Project</a></li>
+                              <li><a href="#" class="nav-link px-2 link-light">Contact</a></li>
+
+                        </ul>
+                  </div>
+            </header>
+
+      </section>
+      <section id="home">
+            <div class="container-fluid">
+                  <div class="row ">
+                        <div class="col-12 col-md-6">
+                              <div class="card">
+                                    <p>ini foto</p>
+                              </div>
+                        </div>
+                        <div class="col-12 col-md-6">
+                              <div class="card">
+                                    <p>ini nama</p>
+                              </div>
+                        </div>
+                  </div>
+            </div>
+      </section>
+
+
+
+
+
+      <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.3.0/dist/js/bootstrap.bundle.min.js" integrity="sha384-geWF76RCwLtnZ8qwWowPQNguL3RmwHVBC9FhGdlKrxdiJJigb/j/68SIy3Te4Bkz" crossorigin="anonymous"></script>
+</body>
+
+</html>
