5061cf88a7c5ae9850c9fb57a912d0d329349f16
rismankadar <kadarismanfisika@gmail.com>
2023-07-28 16:13:57 +0700

    card

--- index.php
+++ index.php
@@ -11,9 +11,9 @@
 </head>
 
 <body>
-      <section>
+      <section class="bg-purple">
 
-            <header class="shadow-sm d-flex flex-wrap sticky-top align-items-center justify-content-center justify-content-md-between py-3 mb-4 border-bottom bg-purple ">
+            <header class="shadow-sm d-flex flex-wrap sticky-top align-items-center justify-content-center justify-content-md-between py-3  border-bottom bg-purple ">
                   <div class="container">
                         <div class="col-md-3 mb-2 mb-md-0">
                               <a href="/" class="d-inline-flex link-body-emphasis text-decoration-none">
@@ -25,7 +25,7 @@
 
                         <ul class="nav col-12 col-md-auto mb-2 justify-content-center mb-md-0 fs-5">
                               <li><a href="#home" class="nav-link px-2 link-secondary">Home</a></li>
-                              <li><a href="#about" class="nav-link px-2 link-light">About</a></li>
+                              <li><a href="#Specialities" class="nav-link px-2 link-light">Specialities</a></li>
                               <li><a href="#" class="nav-link px-2 link-light">Project</a></li>
                               <li><a href="#" class="nav-link px-2 link-light">Contact</a></li>
 
@@ -34,28 +34,28 @@
             </header>
 
       </section>
-      <section id="home">
-            <div class="container-fluid">
+      <section id="home" class="bg-purple">
+            <div class="container-fluid ">
                   <div class="row align-items-center">
                         <div class="col-12 col-md-6 text-center ">
                               <div class="text-center">
 
-                                    <div class="z-3 text-center position-relative">
-                                          <img class="  animate__animated animate__backInLeft" src="img/foto.png" style="width: 300px;" alt="">
+                                    <div class=" text-center ">
+                                          <img class="z-3 rounded-circle position-relative animate__animated animate__backInLeft" src="img/foto.png" style="width: 300px;" alt="">
                                     </div>
                                     <div class="z-2 position-absolute top-0 start-0  animate__animated animate__rotateIn ">
                                           <svg viewBox="0 0 200 200" xmlns="http://www.w3.org/2000/svg" width="700" height="700">
-                                                <path fill="#380637" d="M57.7,-49.8C70.8,-44.6,74.7,-22.3,65.1,-9.6C55.5,3,32.3,6.1,19.2,20.1C6.1,34.2,3,59.2,-5.3,64.5C-13.6,69.7,-27.2,55.3,-31.6,41.2C-35.9,27.2,-31.1,13.6,-32.1,-1.1C-33.2,-15.7,-40.2,-31.5,-35.8,-36.7C-31.5,-42,-15.7,-36.7,3.3,-40C22.3,-43.3,44.6,-55.1,57.7,-49.8Z" transform="translate(100 100)" />
+                                                <path fill="#4b99d8" d="M57.7,-49.8C70.8,-44.6,74.7,-22.3,65.1,-9.6C55.5,3,32.3,6.1,19.2,20.1C6.1,34.2,3,59.2,-5.3,64.5C-13.6,69.7,-27.2,55.3,-31.6,41.2C-35.9,27.2,-31.1,13.6,-32.1,-1.1C-33.2,-15.7,-40.2,-31.5,-35.8,-36.7C-31.5,-42,-15.7,-36.7,3.3,-40C22.3,-43.3,44.6,-55.1,57.7,-49.8Z" transform="translate(100 100)" />
                                           </svg>
                                     </div>
-                                    <div class=" z-1 position-absolute top-0 start-0  animate__animated animate__rotateIn ">
-                                          <svg viewBox="0 0 200 200" xmlns="http://www.w3.org/2000/svg" width="700" height="700">
+                                    <div class=" z-1 position-absolute top-0 start-0  animate__animated animate__rotateIn pt-5 mt-5">
+                                          <svg viewBox="0 0 200 200" xmlns="http://www.w3.org/2000/svg" width="500" height="500">
                                                 <path fill="#08BDBA" d="M30,-38.9C43.3,-24.7,61.4,-19.3,70.6,-6.6C79.8,6.1,80.1,26.3,70.3,38C60.5,49.8,40.5,53.2,21.8,60.1C3,67.1,-14.5,77.6,-25.9,72.7C-37.2,67.9,-42.4,47.7,-41.4,32.2C-40.3,16.8,-33.1,6.1,-33.5,-8.4C-33.9,-22.9,-41.9,-41.2,-37.6,-56.9C-33.3,-72.7,-16.6,-85.9,-4.1,-81C8.4,-76.1,16.8,-53.1,30,-38.9Z" transform="translate(100 100)" />
                                           </svg>
                                     </div>
                               </div>
                         </div>
-                        <div class="col-12 col-md-6 ">
+                        <div class="col-12 col-md-6 text-light z-3 ">
                               <div class="">
                                     <h2> Kadarisman</h2>
                                     <p>"I am a skilled web developer proficient in various technologies. My expertise lies in working with MySQL for database management, PHP for server-side scripting, and PHP framework CodeIgniter 4 for building robust web applications. Additionally, I have a strong grasp of CSS with a focus on utilizing Bootstrap to create visually appealing and responsive user interfaces. With my in-depth knowledge and experience in these technologies, I am capable of developing dynamic and feature-rich websites that cater to modern web development standards."</p>
@@ -65,19 +65,45 @@
                   </div>
             </div>
             <svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 1440 320">
-                  <path fill="#ad84ad" fill-opacity="1" d="M0,96L40,112C80,128,160,160,240,186.7C320,213,400,235,480,224C560,213,640,171,720,170.7C800,171,880,213,960,234.7C1040,256,1120,256,1200,224C1280,192,1360,128,1400,96L1440,64L1440,320L1400,320C1360,320,1280,320,1200,320C1120,320,1040,320,960,320C880,320,800,320,720,320C640,320,560,320,480,320C400,320,320,320,240,320C160,320,80,320,40,320L0,320Z"></path>
+                  <path fill="#4b99d8" fill-opacity="1" d="M0,96L40,112C80,128,160,160,240,186.7C320,213,400,235,480,224C560,213,640,171,720,170.7C800,171,880,213,960,234.7C1040,256,1120,256,1200,224C1280,192,1360,128,1400,96L1440,64L1440,320L1400,320C1360,320,1280,320,1200,320C1120,320,1040,320,960,320C880,320,800,320,720,320C640,320,560,320,480,320C400,320,320,320,240,320C160,320,80,320,40,320L0,320Z"></path>
             </svg>
       </section>
-      <section id="about" class="bg-purple-pastel">
+      <section id="Specialities" class="bg-purple-pastel">
             <div class="container-fluid bg-purple-pastel text-center">
                   <div class="row bg-purple-pastel">
                         <div class="col">
                               <h1 class="text-light">Specialities</h1>
                         </div>
                   </div>
-                  <div class="row">
-                        <div class="col">
-                              <p>deskripsi </p>
+                  <div class="container text-center">
+                        <div class="row justify-content-center">
+                              <div class="col col-md-4">
+                                    <div class="card" style="width: 18rem;">
+                                          <div class="card-body">
+                                                <h5 class="card-title">PHP Native</h5>
+                                                <h6 class="card-subtitle mb-2 text-body-secondary">Card subtitle</h6>
+
+                                          </div>
+                                    </div>
+                              </div>
+                              <div class="col col-md-4">
+                                    <div class="card" style="width: 18rem;">
+                                          <div class="card-body">
+                                                <h5 class="card-title">MySQL</h5>
+                                                <h6 class="card-subtitle mb-2 text-body-secondary">Card subtitle</h6>
+
+                                          </div>
+                                    </div>
+                              </div>
+                              <div class="col col-md-4">
+                                    <div class="card" style="width: 18rem;">
+                                          <div class="card-body">
+                                                <h5 class="card-title">Arduino</h5>
+                                                <h6 class="card-subtitle mb-2 text-body-secondary">Card subtitle</h6>
+
+                                          </div>
+                                    </div>
+                              </div>
                         </div>
                   </div>
             </div>
