252b0751047b7c0301dbccccd25429c0a969ce60
rismankadar <kadarismanfisika@gmail.com>
2023-07-28 11:16:44 +0700

    1 row

--- index.php
+++ index.php
@@ -36,28 +36,38 @@
       </section>
       <section id="home">
             <div class="container-fluid">
-                  <div class="row ">
-                        <div class="col-12 col-md-6 text-center position-relative">
+                  <div class="row align-items-center">
+                        <div class="col-12 col-md-6 text-center ">
                               <div class="text-center">
 
-                                    <div class="z-3 position-absolute top-30 start-50 rounded-3 text-center ">
-                                          <img class="animate__animated animate__backInLeft" src="img/foto.png" style="width: 300px;" alt="">
+                                    <div class="z-3 text-center position-relative">
+                                          <img class="  animate__animated animate__backInLeft" src="img/foto.png" style="width: 300px;" alt="">
                                     </div>
-                                    <div class="z-2 position-absolute animate__animated animate__rotateIn pt-2 mt-2 ">
+                                    <div class="z-2 position-absolute top-0  start-0  animate__animated animate__rotateIn ">
                                           <svg viewBox="0 0 200 200" xmlns="http://www.w3.org/2000/svg" width="700" height="700">
                                                 <path fill="#380637" d="M57.7,-49.8C70.8,-44.6,74.7,-22.3,65.1,-9.6C55.5,3,32.3,6.1,19.2,20.1C6.1,34.2,3,59.2,-5.3,64.5C-13.6,69.7,-27.2,55.3,-31.6,41.2C-35.9,27.2,-31.1,13.6,-32.1,-1.1C-33.2,-15.7,-40.2,-31.5,-35.8,-36.7C-31.5,-42,-15.7,-36.7,3.3,-40C22.3,-43.3,44.6,-55.1,57.7,-49.8Z" transform="translate(100 100)" />
                                           </svg>
                                     </div>
-                                    <div class=" z-1 position-absolute animate__animated animate__rotateIn pt-2 mt-2 ">
+                                    <div class=" z-1 position-absolute top-0 start-0  animate__animated animate__rotateIn ">
                                           <svg viewBox="0 0 200 200" xmlns="http://www.w3.org/2000/svg" width="700" height="700">
                                                 <path fill="#08BDBA" d="M30,-38.9C43.3,-24.7,61.4,-19.3,70.6,-6.6C79.8,6.1,80.1,26.3,70.3,38C60.5,49.8,40.5,53.2,21.8,60.1C3,67.1,-14.5,77.6,-25.9,72.7C-37.2,67.9,-42.4,47.7,-41.4,32.2C-40.3,16.8,-33.1,6.1,-33.5,-8.4C-33.9,-22.9,-41.9,-41.2,-37.6,-56.9C-33.3,-72.7,-16.6,-85.9,-4.1,-81C8.4,-76.1,16.8,-53.1,30,-38.9Z" transform="translate(100 100)" />
                                           </svg>
                                     </div>
                               </div>
                         </div>
-                        <div class="col-12 col-md-6">
-                              <div class="card">
-                                    <p>ini nama</p>
+                        <div class="col-12 col-md-6 ">
+                              <div class="">
+                                    <h2> Kadarisman</h2>
+                                    <p>ini deskripsi</p>
+                                    <p>ini deskripsi</p>
+                                    <p>ini deskripsi</p>
+                                    <p>ini deskripsi</p>
+                                    <p>ini deskripsi</p>
+                                    <p>ini deskripsi</p>
+                                    <p>ini deskripsi</p>
+                                    <p>ini deskripsi</p>
+                                    <p>ini deskripsi</p>
+                                    <p>ini deskripsi</p>
                               </div>
                         </div>
                   </div>
